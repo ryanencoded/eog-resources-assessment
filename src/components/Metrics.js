52e4bf4b11d76433350a565dc7c94c9151606aae
Ryan Thompson <rjthompson@rjthompson.local>
2019-10-07 17:18:18 -0500

    feature: implements logic for handling subscription and adds current metric/range to metrics

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -1,16 +1,44 @@
 import React from "react"
-import { Provider, createClient } from "urql";
+import { SubscriptionClient } from "subscriptions-transport-ws"
+import { Provider, createClient,  defaultExchanges, subscriptionExchange } from "urql";
 import Grid from '@material-ui/core/Grid'
+import RangeSelect from "./RangeSelect"
+import CurrentMetrics from "./CurrentMetrics"
 import MetricsList from "./MetricsList"
 import Chart from "./Chart"
 
+const subscriptionClient = new SubscriptionClient(
+  "ws://react.eogresources.com/graphql",
+  {
+    reconnect: true,
+    timeout: 20000
+  }
+);
+
 const client = createClient({
-  url: "https://react.eogresources.com/graphql"
+  url: "https://react.eogresources.com/graphql",
+  exchanges: [
+    ...defaultExchanges,
+    subscriptionExchange({
+      forwardSubscription: operation => subscriptionClient.request(operation)
+    }),
+  ]
 });
 
 export default () => {
   return (
     <Provider value={client}>
+
+      <Grid container>
+        <Grid item lg={3}>
+          <RangeSelect />
+        </Grid>
+        <Grid item lg={9}>
+          <CurrentMetrics />
+        </Grid>
+      </Grid>
+
+
       <Grid container>
         <Grid item lg={3}>
           <MetricsList />
