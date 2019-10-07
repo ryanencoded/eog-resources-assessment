da7e7e489e027fa71cf77dcadda54f95dcc311b5
Ryan Thompson <rjthompson@rjthompson.local>
2019-10-07 14:09:05 -0500

    feature: adds metric toggle reducer and action

--- src/store/reducers/Metrics.js
+++ src/store/reducers/Metrics.js
@@ -1,18 +1,37 @@
 import * as actions from "../actions";
 
 const initialState = {
-  list: []
+  list: [],
+  selected: []
 };
 
 const metricsListReceived = (state, action) => {
   const { getMetrics } = action;
   return {
+    ...state,
     list: getMetrics
   };
 };
 
+const toggleMetric = (state, action) => {
+  const { metric } = action;
+
+  let selected = state.selected
+  if(selected.indexOf(metric) === -1){
+    selected.push(metric)
+  }else{
+    selected.splice(selected.indexOf(metric), 1)
+  }
+
+  return {
+    ...state,
+    selected: selected
+  };
+};
+
 const handlers = {
-  [actions.METRICS_LIST_RECEIVED]: metricsListReceived
+  [actions.METRICS_LIST_RECEIVED]: metricsListReceived,
+  [actions.METRIC_TOGGLE] : toggleMetric
 };
 
 export default (state = initialState, action) => {
